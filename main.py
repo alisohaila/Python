b047781ddb54aef7f615bff22e3c400c27711fe9
Sohaila Ali <ali.sohailakhalid@gmail.com>
2021-08-14 23:03:05 -0400

    added more questions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ else:
     quit()
 
 score = 0
-num_of_questions = 3
+num_of_questions = 5
 
 question = input("1. Is Python a creature or a programming language? ").lower()
 if question == "programming language":
@@ -24,18 +24,54 @@ else:
 
 answer = "decimal number"
 question = input("2. What is a float in Python, JavaScript, Java, and Ruby? (even number/decimal number)  ").lower()
-if question != answer:
-    print("Incorrect! It is decimal number")
+if question == answer:
+    print("Correct! ")
+    score+= 1
+elif question == "even number":
+    print("Incorrect! It is decimal number") 
 else:
-    print("Correct! 1+")
-    score += 1
+    print("Invalid Input")
+    quit()
+
 
 answer = 'web development'
 question = input("3. What is HTML used for? (Game development/web development/app development) ").lower()
 if question == answer:
     print("Correct! 1+")
     score += 1
-else:
+elif question == "game development":
     print("Incorrect! It is Web development! ")
+elif question == "app development":
+    print("Incorrect! It is Web development! ")    
+else:
+    print("Invalid Input")
+    quit()
+   
+answer = "Object Oriented Programming"
+question = input("4. What does OOP stand for? (Out of Print/Object Oriented Programming/Out Of Place)").lower()
+if question == answer:
+    print("Correct! 1+")
+    score+= 1
+elif question == "Out of print":
+    print("Incorrect! It is Object Oriented Programming")
+elif question == "Out of place":
+    print("Incorrect! It is Object Oriented Programming")
+else:
+    print("Invalid Input")
+    quit()
+
+
+answer = "see-sharp"            
+question = input("5. How do you pronounce C#? (see-hashtag/see-sharp)").lower()
+if question == answer:
+    print("Correct! 1+")
+    score+= 1
+elif question == "see-hashtag":
+    print("Incorrect! It's see-sharp")
+else:
+    print("Invalid Input")
+    quit()
+
+
 
 print("You've got " + str(score) + "/" + str(num_of_questions))
